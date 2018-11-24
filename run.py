80064cd45dfeb4d5e6c99cfafff8be32d141e346
Artemis <Osmosis.orb@gmail.com>
2018-11-24 10:43:55 +0000

    Fix bug

--- run.py
+++ run.py
@@ -7,18 +7,18 @@ try:
     querystring = {"market":"GB"}
     headers = {'Accept': "application/json",'Content-Type': "application/json",'Authorization': OAuth}
     r=requests.request("GET", url, headers=headers, params=querystring).json()
-    playlist_length = int(r["tracks"]["total"])
     while True:
         try:
-            url = "https://api.spotify.com/v1/playlists/{}tracks".format(Playlist)
-            x=rng().randrange(0, 32)
-            a=int(playlist_length/10)
-            y=rng().randrange(0, a)
-            payload = {"range_start":0,"range_length":y,"insert_before":x}
+            url = "https://api.spotify.com/v1/playlists/{}/tracks".format(Playlist)
+            x=rng().randrange(0, int(r["tracks"]["total"]/10))
+            y=rng().randrange(0, 64)
+            payload = {"range_start":1,"range_length":y,"insert_before":x}
             headers = {'Accept': "application/json",'Content-Type': "application/json",'Authorization': OAuth}
-            requests.request("PUT", url, data=payload, headers=headers)
+            a=requests.request("PUT", url, data=json.dumps(payload), headers=headers).json()
+            print("{} songs moved to {}-{}".format(x, y, y+x))
             time.sleep(5)
-        except:
+        except Exception as e:
+            print("Error: {}\n{}".format(e, traceback.format_exc()))
             pass #prevents errors due to exceeding rate-limits
 except Exception as e:
     print("Error: {}\n{}".format(e, traceback.format_exc()))
