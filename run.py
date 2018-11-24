95fca9da140c0ffc616b7b41c9a86f8af1c615a9
Artemis <Osmosis.orb@gmail.com>
2018-11-24 13:37:05 +0000

    Re-updated error logging

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 import asyncio, json, random, requests, time, traceback, os
 rng = random.SystemRandom
 try:
+    os.system('cls')
     print("Go here and get a OAuth Token: https://developer.spotify.com/console/put-playlist-tracks/")
     print("Paste your token here: ")
     OAuth = input()
     OAuth = "Bearer {}".format(OAuth)
+    os.system('cls')
     print("Paste your playlist URL here: ")
     Playlist = input()
     Playlist = Playlist.split("/")[6].split("?")[0]
@@ -23,12 +25,21 @@ try:
             x=rng().randrange(1, plc)
             payload = {"range_start":0,"range_length":1,"insert_before":x}
             headers = {'Accept': "application/json",'Content-Type': "application/json",'Authorization': OAuth}
-            a=requests.request("PUT", url, data=json.dumps(payload), headers=headers).json()
-            time.sleep(0.2)
+            r=requests.request("PUT", url, data=json.dumps(payload), headers=headers)
+            json = json.loads(r.text)
+            if r.ok:
+                time.sleep(0.25)
+            else:
+                time.sleep(2)
+            if r.status_code == 403:
+                os.system('cls')
+                print("Error: {}".format(json['error']['message']))
+                break
             os.system('cls')
-            print("{}/{}".format(i,plc))
+            print("{}/{} | Status: {}\n \n{}".format(i,plc,r.status_code))
         except Exception as e:
-            print("Error: {}\n{}".format(e, traceback.format_exc()))
+            print(e)
+            time.sleep(5)
             pass #prevents errors due to exceeding rate-limits
 except Exception as e:
     print("Error: {}\n{}".format(e, traceback.format_exc()))
