0f59f2f5caa9c7cdb695fb6e817c2e276f63db44
Artemis <Osmosis.orb@gmail.com>
2018-11-25 12:52:42 +0000

    Fixing bug with variable name

--- run.py
+++ run.py
@@ -2,13 +2,10 @@ import asyncio, json, random, requests, time, traceback, os
 rng = random.SystemRandom
 try:
     os.system('cls')
-    print("Go here and get a OAuth Token: https://developer.spotify.com/console/put-playlist-tracks/")
-    print("Paste your token here: ")
-    OAuth = input()
+    OAuth = input("Go here and get a OAuth Token: https://developer.spotify.com/console/put-playlist-tracks/\nPaste your token here: ")
     OAuth = "Bearer {}".format(OAuth)
     os.system('cls')
-    print("Paste your playlist URL here: ")
-    Playlist = input()
+    Playlist = input("Paste your playlist URL here: ")
     Playlist = Playlist.split("/")[6].split("?")[0]
     try:
         url = "https://api.spotify.com/v1/playlists/{}".format(Playlist)
@@ -26,19 +23,18 @@ try:
             payload = {"range_start":0,"range_length":1,"insert_before":x}
             headers = {'Accept': "application/json",'Content-Type': "application/json",'Authorization': OAuth}
             r=requests.request("PUT", url, data=json.dumps(payload), headers=headers)
-            json = json.loads(r.text)
+            x = json.loads(r.text)
             time.sleep(0.25)
             if r.status_code == 403:
                 os.system('cls')
-                print("Error: {}".format(json['error']['message']))
+                print("Error: {}".format(x['error']['message']))
                 break
             os.system('cls')
             print("{} of {} songs randomised.".format(i,plc))
         except Exception as e:
-            print(e)
+            print("Error: {}\n{}".format(e, traceback.format_exc()))
             time.sleep(5)
             pass #prevents errors due to exceeding rate-limits
 except Exception as e:
     print("Error: {}\n{}".format(e, traceback.format_exc()))
  
-    
